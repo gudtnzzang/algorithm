ce1a2e734162ec2381c92a2e27b27707f73720f1
gudtnzzang <hs950823@gmail.com>
2021-06-08 22:35:00 +0900

    전화번호 목록 추가

--- programmers/Hash/전화번호 목록/src/Solution.java
+++ programmers/Hash/전화번호 목록/src/Solution.java
@@ -1,3 +1,21 @@
-public class Solution {
+import java.util.Arrays;
 
+public class Solution {
+    public static void main(String[] args) {
+        String[] phone_book = {"14","88","1235","12","5"};
+        System.out.println(solution(phone_book));
+    }
+    public static boolean solution(String[] phone_book) {
+        boolean answer = true;
+        Arrays.sort(phone_book);
+
+        for(int i=0; i<phone_book.length - 1; i++) {
+            if(phone_book[i+1].startsWith(phone_book[i])) {
+                answer = false;
+                break;
+            }
+        }
+
+        return answer;
+    }
 }
