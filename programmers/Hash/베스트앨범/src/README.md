bc88e4cdcb13b60c3599cb99dc31149af75c151c
hyungsoobae <hs950823@gmail.com>
2021-06-11 17:57:27 +0900

    베스트앨범 임시

--- programmers/Hash/베스트앨범/src/README.md
+++ programmers/Hash/베스트앨범/src/README.md
@@ -1,4 +1,4 @@
-# 완주하지 못한 선수
+# 베스트앨범
 * [문제내용](https://programmers.co.kr/learn/courses/30/lessons/42579)
   </br></br>
 ## 코드 v1
--- programmers/Hash/베스트앨범/src/Solution.java
+++ programmers/Hash/베스트앨범/src/Solution.java
@@ -5,21 +5,37 @@ public class Solution {
     public static void main(String[] args) {
         String[] genres = {"classic", "pop", "classic", "classic", "pop"};
         int[] plays = {500, 600, 150, 800, 2500};
-        solution(genres,plays);
+        int[] answer = solution(genres,plays);
+        for(int a : answer) {
+            System.out.println(a);
+        }
     }
     public static int[] solution(String[] genres, int[] plays) {
         int[] answer = {};
-
+        int cnt = 0;
         HashMap<String, Integer> hm = new HashMap<>();
 
         for(int i=0; i<genres.length; i++) {
             hm.put(genres[i],hm.getOrDefault(genres[i],0) + plays[i]);
         }
+        answer = new int[hm.size() * 2];
+        for(int i=0; i<answer.length / 2; i++) {
+            String maxKey = Collections.max(hm.entrySet(), HashMap.Entry.comparingByValue()).getKey();
+            hm.remove(maxKey);
 
-        String maxKey = Collections.max(hm.entrySet(), HashMap.Entry.comparingByValue()).getKey();
+            HashMap<Integer, Integer> index = new HashMap<>();
+            for (int j = 0; j < genres.length; j++) {
+                if (genres[j] == maxKey) {
+                    index.put(j, plays[j]);
+                }
+            }
 
-
-
+            for(int l=0; l<2; l++) {
+                Integer maxKey2 = Collections.max(index.entrySet(), HashMap.Entry.comparingByValue()).getKey();
+                index.remove(maxKey2);
+                answer[cnt++] = maxKey2;
+            }
+        }
         return answer;
     }
 }
